4ed679afe2ddb951cb2640674f05aefce75e2907
tonitoelgalo <adirat@free.fr>
2017-12-21 09:59:51 +0100

    Add files via upload

---
+++ app-corr.js
@@ -0,0 +1,52 @@
+/*
+	Méthode 1
+	Avec le formulaire et les inputs "text".
+
+	Pour la méthode avec les boutons, voir app2.js
+*/
+"use strict";
+(function(){
+	console.log("Bienvenue dans le fichier app.js");
+
+
+	// Je teste que jQuery marche bien
+	// Je récupère le texte de ma balise h1
+	var valH1 = $("h1").text();
+	console.log("Mon h1 est '" + valH1 + "'.");
+
+
+	// Si des erreurs dans les inputs,
+	// afficher un message d'erreur
+
+	// Détecter quand le formulaire est envoyé
+	$("#buttonForm").click(clickButtonFormulaire);
+
+	function clickButtonFormulaire(){
+		
+	}
+
+	function calculer(nb1, nb2, operation){
+		
+		}
+
+		return res;
+	}
+
+
+	function add(a, b){
+		
+	}	
+
+	function substract(a, b){
+		
+	}	
+
+	function multiply(a, b){
+		
+	}	
+
+	function divide(a, b){
+		
+	}	
+
+})();
